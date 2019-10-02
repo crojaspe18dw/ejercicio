cb405341db85e87192d4d24863cecc955e41c8c4
crojaspe18dw <crojaspe18dw@ikzubirimanteo.com>
2019-10-02 08:48:22 +0200

    github del ejercicio para que otros grupos practiquen

---
+++ ejercicioconjefry.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+  <title></title>
+ </head>
+ <body>
+
+ <form>
+    <input type="number" name="mes" value="introduzca el mes en el que nacio">
+    <input type="number" name="dia" value="introduzca el dia en el que nacio">
+    <input type="button" value="calcular" click="calcular()">
+ </form>
+
+
+ <?php 
+
+
+?>
+
+ </body>
+</html>
